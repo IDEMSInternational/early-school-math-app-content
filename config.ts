f951a194b6f9eb4792634d71e02cfb8db826e1df
Chris Wright <chris@kitchentablemath.com>
2025-05-12 10:50:02 -0700

    content: 1.1.69

--- config.ts
+++ config.ts
@@ -8,7 +8,7 @@ config.google_drive = {
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/early-school-math-app-content.git",
-  content_tag_latest: "1.1.68",
+  content_tag_latest: "1.1.69",
 };
 
 config.android = {
