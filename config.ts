306e0052f2dbf546656f7055f91b8def43b98a4f
Chris Marsh <84872334+ChrisMarsh82@users.noreply.github.com>
2026-08-28 13:41:32 +0100

    Update content_tag_latest to version 1.1.200

--- config.ts
+++ config.ts
@@ -8,7 +8,7 @@ config.google_drive = {
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/early-school-math-app-content.git",
-  content_tag_latest: "1.1.199",
+  content_tag_latest: "1.1.200",
 };
 
 config.android = {
